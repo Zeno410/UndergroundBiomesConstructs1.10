16c648f1e98a8c2f6cea69d36ab437b206b8502c
Zeno410 <zeno410@aol.com>
2017-01-17 21:23:32 -0800

    Recipe fixes
    Some weren't written properly

--- src/main/java/exterminatorjeff/undergroundbiomes/common/RegularStoneRecipe.java
+++ src/main/java/exterminatorjeff/undergroundbiomes/common/RegularStoneRecipe.java
@@ -9,8 +9,10 @@ import com.google.common.collect.Lists;
 
 import exterminatorjeff.undergroundbiomes.api.common.UBLogger;
 import exterminatorjeff.undergroundbiomes.api.enums.UBStoneStyle;
+import exterminatorjeff.undergroundbiomes.api.names.StoneEntry;
 import exterminatorjeff.undergroundbiomes.config.SettingTracker;
 import exterminatorjeff.undergroundbiomes.intermod.StonesRegistry;
+import java.util.function.Consumer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -18,6 +20,7 @@ import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.ShapedRecipes;
 import net.minecraft.item.crafting.ShapelessRecipes;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 /**
  * 
@@ -36,19 +39,24 @@ public final class RegularStoneRecipe implements SettingTracker<Integer> {
 		// Remove previous recipes
 		CraftingManager.getInstance().getRecipeList().removeAll(recipes);
 		// Create the new recipes
-		StonesRegistry.INSTANCE.stonesFor(UBStoneStyle.COBBLE).forEach(cobbleEntry -> {
-			ItemStack cobble = new ItemStack(cobbleEntry.getItemBlock());
-			switch (value) {
-			case 1:
-				recipes.add(new ShapelessRecipes(new ItemStack(Blocks.COBBLESTONE), Lists.newArrayList(cobble)));
-			case 2:
-				recipes.add(new ShapelessRecipes(new ItemStack(Blocks.COBBLESTONE), Lists.newArrayList(cobble, new ItemStack(Items.REDSTONE))));
-			case 3:
-				recipes.add(new ShapedRecipes(2, 2, new ItemStack[] { cobble, cobble, cobble, cobble }, new ItemStack(Blocks.COBBLESTONE)));
-			case 4:
-				recipes.add(new ShapedRecipes(2, 2, new ItemStack[] { cobble, cobble, cobble, cobble }, new ItemStack(Blocks.COBBLESTONE, 4)));
-			}
-		});
+		StonesRegistry.INSTANCE.stonesFor(UBStoneStyle.COBBLE).forEach(new Consumer<StoneEntry>() {
+                    @Override
+                    public void accept(StoneEntry cobbleEntry) {
+                        ItemStack cobble = new ItemStack(cobbleEntry.getItemBlock());
+                        switch (value) {
+                            case 1:
+                                recipes.add(new ShapelessRecipes(new ItemStack(Blocks.COBBLESTONE), Lists.newArrayList(cobble)));
+                            case 2:
+                                recipes.add(new ShapelessRecipes(new ItemStack(Blocks.COBBLESTONE), Lists.newArrayList(cobble, new ItemStack(Items.REDSTONE))));
+                            case 3:
+                                recipes.add(new ShapedOreRecipe(new ItemStack(Blocks.COBBLESTONE, 1), "XX", "XX", 'X', "cobblestone"));
+//new ShapedRecipes(2, 2, new ItemStack[] { cobble, cobble, cobble, cobble }, new ItemStack(Blocks.COBBLESTONE)));
+                            case 4:
+                                recipes.add(new ShapedOreRecipe(new ItemStack(Blocks.COBBLESTONE, 4), "XX", "XX", 'X', "cobblestone"));
+                                //new ShapedRecipes(2, 2, new ItemStack[] { cobble, cobble, cobble, cobble }, new ItemStack(Blocks.COBBLESTONE, 4)));
+                        }
+                    }
+                });
 		// Add the new recipes
 		CraftingManager.getInstance().getRecipeList().addAll(recipes);
 	}
