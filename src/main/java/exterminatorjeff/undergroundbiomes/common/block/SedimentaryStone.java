1363c0f5fd6bd38151677a710c36946cf97c5c7c
Zeno410 <zeno410@aol.com>
2017-01-17 21:14:12 -0800

    Lignite Drop fix
    Returns appropriate met

--- src/main/java/exterminatorjeff/undergroundbiomes/common/block/SedimentaryStone.java
+++ src/main/java/exterminatorjeff/undergroundbiomes/common/block/SedimentaryStone.java
@@ -1,5 +1,6 @@
 package exterminatorjeff.undergroundbiomes.common.block;
 
+import com.google.common.base.Predicate;
 import static exterminatorjeff.undergroundbiomes.api.enums.SedimentaryVariant.*;
 
 import java.util.Random;
@@ -7,12 +8,15 @@ import java.util.Random;
 import exterminatorjeff.undergroundbiomes.api.API;
 import exterminatorjeff.undergroundbiomes.api.enums.UBStoneStyle;
 import exterminatorjeff.undergroundbiomes.api.enums.UBStoneType;
+import exterminatorjeff.undergroundbiomes.intermod.OresRegistry;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 /**
@@ -63,11 +67,20 @@ public class SedimentaryStone extends UBStone {
 
 	@Override
 	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-		if (state.getValue(SEDIMENTARY_VARIANT_PROPERTY) == LIGNITE)
+		if (state.getValue(SEDIMENTARY_VARIANT_PROPERTY) == LIGNITE) {
 			return API.LIGNITE_COAL.getItem();
+                }
 		else
 			return super.getItemDropped(state, rand, fortune);
 	}
+        
+	@Override
+	public int damageDropped(IBlockState state) {
+		if (state.getValue(SEDIMENTARY_VARIANT_PROPERTY) == LIGNITE) {
+                    return 0;
+                }
+		return super.damageDropped(state);
+	}        
 
 	@Override
 	public boolean isFortuneAffected(IBlockState state) {
@@ -83,5 +96,10 @@ public class SedimentaryStone extends UBStone {
 	public float getExplosionResistance(World world, BlockPos pos, Entity exploder, Explosion explosion) {
 		return getBaseResistance() * world.getBlockState(pos).getValue(SEDIMENTARY_VARIANT_PROPERTY).getResistance();
 	}
-
+        
+    @Override
+    public boolean isReplaceableOreGen(IBlockState state, IBlockAccess world, BlockPos pos, Predicate<IBlockState> target) {
+        OresRegistry.INSTANCE.setRecheck(world, pos);
+        return super.isReplaceableOreGen(state, world, pos, target); //To change body of generated methods, choose Tools | Templates.
+    }
 }
