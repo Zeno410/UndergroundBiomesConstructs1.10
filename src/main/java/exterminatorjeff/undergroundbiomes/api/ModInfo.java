7f6251c0f867f7020f9e4b883fabeecd11d2b338
Zeno410 <zeno410@aol.com>
2017-02-04 11:25:22 -0800

    Version update
    to beta.15

--- src/main/java/exterminatorjeff/undergroundbiomes/api/ModInfo.java
+++ src/main/java/exterminatorjeff/undergroundbiomes/api/ModInfo.java
@@ -10,7 +10,7 @@ public final class ModInfo {
 
 	public static final String MODID = "undergroundbiomes";
 	public static final String NAME = "Underground Biomes";
-	public static final String VERSION = "0.9.beta.8";
+	public static final String VERSION = "0.9.beta.15";
 
 	private ModInfo() {
 	}
