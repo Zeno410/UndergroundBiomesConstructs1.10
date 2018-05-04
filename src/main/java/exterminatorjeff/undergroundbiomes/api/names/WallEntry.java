5c5b72899246e2cc615a67b2a2b6ddf3cdb92b6b
kyeiti <kyeiti@vorsicht-bissig.de>
2018-05-04 16:24:31 +0200

    Fixed wall names

--- src/main/java/exterminatorjeff/undergroundbiomes/api/names/WallEntry.java
+++ src/main/java/exterminatorjeff/undergroundbiomes/api/names/WallEntry.java
@@ -16,11 +16,13 @@ public final class WallEntry extends BlockEntry {
 
   @Override
   protected void doRegisterItem(IForgeRegistry<Item> registry) {
+    getItemBlock().setUnlocalizedName(internalName);
     registry.register(getItemBlock().setRegistryName(internalName));
   }
 
   @Override
   protected void doRegisterBlock(IForgeRegistry<Block> registry) {
+    getBlock().setUnlocalizedName(internalName);
     getBlock().setRegistryName(internalName);
     registry.register(getBlock());
   }
