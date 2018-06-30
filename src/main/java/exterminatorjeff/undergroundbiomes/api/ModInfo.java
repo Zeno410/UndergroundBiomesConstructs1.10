66c18e80dda4403ed1d35e6f5ca63f3fb5d360cc
kyeiti <kyeiti@vorsicht-bissig.de>
2018-06-30 19:35:47 +0200

    Version bump

--- src/main/java/exterminatorjeff/undergroundbiomes/api/ModInfo.java
+++ src/main/java/exterminatorjeff/undergroundbiomes/api/ModInfo.java
@@ -9,7 +9,7 @@ public final class ModInfo {
 
   public static final String MODID = "undergroundbiomes";
   public static final String NAME = "Underground Biomes";
-  public static final String VERSION = "1.2.0";
+  public static final String VERSION = "1.2.1";
 
   private ModInfo() {
   }
